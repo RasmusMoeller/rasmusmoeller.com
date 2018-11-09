e0edf38b6c48bf3355fedc33beaf9746b122f8c8
Rasmus Kokholm Møller <rasmus-moeller@hotmail.com>
2018-11-09 15:43:33 +0100

    Added Login Page + Changed Sass Variables

---
+++ login.php
@@ -0,0 +1,33 @@
+<html lang="en" dir="ltr">
+  <?php include "includes/head.php" ?>
+  <body>
+
+    <div class="nav navbg">
+      <ul>
+        <a href="index.php"><li class="active">Home</li></a>
+        <a href="portfolio.php"><li>Portfolio</li></a>
+        <a href="contact.php"><li>Contact</li></a>
+      </ul>
+    </div>
+
+    <i class="fas fa-bars"></i>
+    <div class="nav_collapsed">
+      <ul>
+        <a href="index.php"><li class="active">Home</li></a>
+        <a href="portfolio.php"><li>Portfolio</li></a>
+        <a href="contact.php"><li>Contact</li></a>
+      </ul>
+    </div>
+
+    <div class="login_page">
+      <div class="login_wrapper">
+        <h1>Login</h1>
+        <form action="#" method="post" autocomplete="false">
+          <input type="text" name="email" placeholder="Email" required>
+          <input type="password" name="password" placeholder="Password" required>
+          <input type="submit" value="Log In">
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
